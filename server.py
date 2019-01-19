0b6af16292631827b591335a0f1a83a077bd40ae
louis <louis.antonini@epitech.eu>
2019-01-19 23:08:01 +0100

    Initiate Flask template.

--- server.py
+++ server.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from flask import request
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/login', methods=['GET'])
+def login():
+    error = None
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    return render_template('hello.html', error=error)
