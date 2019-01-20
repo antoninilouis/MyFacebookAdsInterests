e553fd7f190a9670848bee5b482c0a0047f161f7
louis <louis.antonini@epitech.eu>
2019-01-20 01:45:01 +0100

    Make autocomplete route.

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import request
 from flask import render_template
+from flask import json
 
 from facebook_business.api import FacebookAdsApi
 from facebook_business.adobjects.targetingsearch import TargetingSearch
@@ -8,17 +9,18 @@ from facebook_business.adobjects.targetingsearch import TargetingSearch
 app = Flask(__name__)
 app.config.from_envvar('APP_CONFIG')
 
+# Setup the API calls
+app_id = app.config['APP_ID']
+app_secret = app.config['APP_SECRET']
+access_token = app.config['ACCESS_TOKEN']
+FacebookAdsApi.init(app_id, app_secret, access_token)
+
 @app.route('/')
 def main():
     return render_template('main.html')
 
 @app.route('/seed_list', methods=['POST', 'GET'])
 def seed_list():
-    app_id = app.config['APP_ID']
-    app_secret = app.config['APP_SECRET']
-    access_token = app.config['ACCESS_TOKEN']
-    FacebookAdsApi.init(app_id, app_secret, access_token)
-
     interest_list = [request.form['interest-1'], request.form['interest-2'], request.form['interest-3']]
 
     results = TargetingSearch.search(params={
@@ -30,3 +32,22 @@ def seed_list():
     if request.method == 'POST':
         return render_template('seed_list.html', results=results)
     return render_template('seed_list.html')
+
+@app.route('/adinterest')
+def adinterest():
+    term = request.args.get('term')
+
+    items = TargetingSearch.search(params={
+        'q': term,
+        'type': TargetingSearch.TargetingSearchTypes.interest,
+        'limit': 10,
+    })
+
+    results = list(map(lambda x: x.get('name'), items))
+
+    response = app.response_class(
+        response = json.dumps(results),
+        status = 200,
+        mimetype = 'application/json'
+    )
+    return response
