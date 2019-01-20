364e531b74339c5b5427fd66e5dacffc516c8542
louis <louis.antonini@epitech.eu>
2019-01-20 01:15:06 +0100

    Setup configuration and calls to Graph API.

--- server.py
+++ server.py
@@ -2,7 +2,11 @@ from flask import Flask
 from flask import request
 from flask import render_template
 
+from facebook_business.api import FacebookAdsApi
+from facebook_business.adobjects.targetingsearch import TargetingSearch
+
 app = Flask(__name__)
+app.config.from_envvar('APP_CONFIG')
 
 @app.route('/')
 def main():
@@ -10,7 +14,19 @@ def main():
 
 @app.route('/seed_list', methods=['POST', 'GET'])
 def seed_list():
+    app_id = app.config['APP_ID']
+    app_secret = app.config['APP_SECRET']
+    access_token = app.config['ACCESS_TOKEN']
+    FacebookAdsApi.init(app_id, app_secret, access_token)
+
+    interest_list = [request.form['interest-1'], request.form['interest-2'], request.form['interest-3']]
+
+    results = TargetingSearch.search(params={
+        'interest_list': interest_list,
+        'type': TargetingSearch.TargetingSearchTypes.interest_suggestion,
+        'limit': 45,
+    })
+
     if request.method == 'POST':
-        request.form['interest-1']
-        return render_template('seed_list.html', interests=request.form.lists())
+        return render_template('seed_list.html', results=results)
     return render_template('seed_list.html')
